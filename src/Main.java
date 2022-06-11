aa7691c25a5095dd020f518fc9799d6ad8631d8f
ArtyomSirko <asmatrix-1@mail.ru>
2022-06-11 11:22:29 +0300

    сдал домашнию работу 3 добавил 2 задания

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,38 @@ public class Main {
         }
 
 
+        int deliveryDistance = 847;  //4 задача
+        int days = (deliveryDistance + 20) / 40 + 1;
+        if (deliveryDistance < 20) {
+            System.out.println("Срок достаки один день");
+        } else {
+            System.out.println("Срок доставки " + days + " дней");
+
+            int monthNumber = 1;   //5 задача
+            switch (monthNumber) {
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println("зимний месяц");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("весенний месяц");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("летний месяц");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("осений месяц");
+                    break;
+                default:
+                    System.out.println("нет такого месяца");
+            }
+        }
     }
 }
